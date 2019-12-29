536c4c677719e315bad192b6c7f88d9b704e6551
shimizu.kenta <shimizukentagm1@gmail.com>
2019-12-30 02:12:25 +0900

    2019:12:30T02:12

--- src/main/java/http/util/HttpServerServiceConfig.java
+++ src/main/java/http/util/HttpServerServiceConfig.java
@@ -9,48 +9,48 @@ abstract public class HttpServerServiceConfig {
 	
 	public static final boolean defaultAddTimestamps = true;
 	
-	private final Collection<String> hostNames = new HashSet<>();
-	private String serverName;
-	private boolean addTimestamps;
+	private final Collection<String> _hostNames = new HashSet<>();
+	private String _serverName;
+	private boolean _addTimestamps;
 
 	public HttpServerServiceConfig() {
-		this.serverName = null;
-		this.addTimestamps = defaultAddTimestamps;
+		this._serverName = null;
+		this._addTimestamps = defaultAddTimestamps;
 	}
 	
 	public Collection<String> hostNames() {
 		synchronized ( this ) {
-			return Collections.unmodifiableCollection(hostNames);
+			return Collections.unmodifiableCollection(_hostNames);
 		}
 	}
 	
 	public boolean hostName(CharSequence cs) {
 		synchronized ( this ) {
-			return this.hostNames.add(cs.toString());
+			return this._hostNames.add(cs.toString());
 		}
 	}
 	
 	public Optional<String> serverName() {
 		synchronized ( this ) {
-			return serverName == null ? Optional.empty() : Optional.of(serverName);
+			return _serverName == null ? Optional.empty() : Optional.of(_serverName);
 		}
 	}
 	
 	public void serverName(CharSequence cs) {
 		synchronized ( this ) {
-			this.serverName = cs.toString();
+			this._serverName = cs.toString();
 		}
 	}
 	
 	public boolean addTimestamps() {
 		synchronized ( this ) {
-			return addTimestamps;
+			return _addTimestamps;
 		}
 	}
 	
 	public void addTimestamps(boolean f) {
 		synchronized ( this ) {
-			this.addTimestamps = f;
+			this._addTimestamps = f;
 		}
 	}
 	
