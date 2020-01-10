f2824b0c7124561549102f7f16926f26d0ead33f
shimizu.kenta <shimizukentagm1@gmail.com>
2020-01-10 21:59:11 +0900

    2020:01:10T21:59

--- src/main/java/http/util/HttpServerService.java
+++ src/main/java/http/util/HttpServerService.java
@@ -1,7 +1,11 @@
 package http.util;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+import http.HttpLog;
+import http.HttpLogListener;
 import http.HttpMessageParseException;
 import http.HttpServerServiceSupplier;
 import http.base.HttpRequestMessage;
@@ -43,5 +47,24 @@ abstract public class HttpServerService implements HttpServerServiceSupplier {
 		}
 	}
 	
-
+	private final Collection<HttpLogListener> logListeners = new CopyOnWriteArrayList<>();
+	
+	public boolean addLogListener(HttpLogListener lstnr) {
+		return logListeners.add(lstnr);
+	}
+	
+	public boolean removeLogListener(HttpLogListener lstnr) {
+		return logListeners.remove(lstnr);
+	}
+	
+	protected void putLog(HttpLog log) {
+		logListeners.forEach(lstnr -> {
+			lstnr.receive(log);
+		});
+	}
+	
+	protected void putLog(Throwable t) {
+		putLog(new HttpLog(t));
+	}
+	
 }
